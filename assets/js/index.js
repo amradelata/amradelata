010e387984f1ad254baf6a570fc36a24fd816a1b
amradelata <amradelata@gmail.com>
2019-11-06 10:25:05 +0200

    phoneImage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 let allCards = document.querySelectorAll('.my-card');
 let buttons = document.querySelectorAll('button')
+let phonePars = document.getElementById('phonePars')
+let topbtn = document.querySelector(".top-button")
 
 buttons.forEach(category => category.addEventListener('click', function (){
    document.querySelector('.top-button .active').classList.remove('active');
@@ -19,3 +21,10 @@ buttons.forEach(category => category.addEventListener('click', function (){
 
 	})
 }))
+
+phonePars.addEventListener('click', () =>{
+	// topbtn.style.display = "none !important"
+	topbtn.style.display = "block !important";
+	topbtn.classList.toggle("heid")
+	console.log(topbtn)
+})
