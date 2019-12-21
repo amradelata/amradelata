a075e8d337da61d5e870860bbfb4d013438bf49b
amradelata <amradelata@gmail.com>
2019-12-21 11:32:35 +0200

    get 100 in seo

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,39 +3,19 @@ let buttons = document.querySelectorAll('button')
 let phonePars = document.getElementById('phonePars')
 let topbtn = document.querySelector(".top-buttonphone")
 
-buttons.forEach(category => category.addEventListener('click', function (){
-   document.querySelector('.top-button .active').classList.remove('active');
-        category.classList.add('active');
+buttons.forEach(category => category.addEventListener('click', function () {
+	document.querySelector('.top-button .active').classList.remove('active');
+	category.classList.add('active');
 
-	allCards.forEach(card =>{
+	allCards.forEach(card => {
 
-		if(category.id === 'all'){
+		if (category.id === 'all') {
 			card.classList.remove('hide')
 			return;
 		}
-		if(category.id !== card.getAttribute('data-name') ){
+		if (category.id !== card.getAttribute('data-name')) {
 			card.classList.add('hide')
-		}else{
-			card.classList.remove('hide')
-		}
-
-	})
-}))
-
-
-buttons.forEach(category => category.addEventListener('click', function (){
-   document.querySelector('.top-buttonphone .active').classList.remove('active');
-        category.classList.add('active');
-
-	allCards.forEach(card =>{
-
-		if(category.id === 'all'){
-			card.classList.remove('hide')
-			return;
-		}
-		if(category.id !== card.getAttribute('data-name') ){
-			card.classList.add('hide')
-		}else{
+		} else {
 			card.classList.remove('hide')
 		}
 
@@ -44,7 +24,29 @@ buttons.forEach(category => category.addEventListener('click', function (){
 
 
 
-phonePars.addEventListener('click', () =>{
+
+buttons.forEach(category => category.addEventListener('click', function () {
+	// document.querySelector('.top-buttonphone .active').classList.remove('active');
+	category.classList.add('active');
+
+	allCards.forEach(card => {
+
+		if (category.id === 'all') {
+			card.classList.remove('hide')
+			return;
+		}
+		if (category.id !== card.getAttribute('data-name')) {
+			card.classList.add('hide')
+		} else {
+			card.classList.remove('hide')
+		}
+
+	})
+}))
+
+
+
+phonePars.addEventListener('click', () => {
 	// topbtn.style.display = "none !important"
 	topbtn.style.display = "block !important";
 	topbtn.classList.toggle("heid")
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,32 @@
 window.onload = () => {
-    scrollToProjects();
-    projectsTabs();
-    lazyLoadImages();
-    updateProjecthumbnails();
+	scrollToProjects();
+	projectsTabs();
+	lazyLoadImages();
+	updateProjecthumbnails();
 };
 
 let allCards = document.querySelectorAll('.my-card');
 let buttons = document.querySelectorAll('button')
 
-buttons.forEach(category => category.addEventListener('click', function (){
-   document.querySelector('.top-button .active').classList.remove('active');
-        category.classList.add('active');
+buttons.forEach(category => category.addEventListener('click', function () {
+	document.querySelector('.top-button .active').classList.remove('active');
+	category.classList.add('active');
 
-	allCards.forEach(card =>{
+	allCards.forEach(card => {
 
-		if(category.id === 'all'){
+		if (category.id === 'all') {
 			card.classList.remove('hide')
 			return;
 		}
-		if(category.id !== card.getAttribute('data-name') ){
+		if (category.id !== card.getAttribute('data-name')) {
 			card.classList.add('hide')
-		}else{
+		} else {
 			card.classList.remove('hide')
 		}
 
 	})
 }))
+
 
 // function lazyLoadImages() {
 //     const lazyImages = document.querySelectorAll('.project_image');
