cb70a2fe83e60b57f6b454d8d87bb335b6b41a38
amradelata <amradelata@gmail.com>
2019-12-25 16:28:06 +0200

    Minifyhtml/css

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,23 @@ let allCards = document.querySelectorAll('.my-card');
 let buttons = document.querySelectorAll('button')
 let phonePars = document.getElementById('phonePars')
 let topbtn = document.querySelector(".top-buttonphone")
-
 buttons.forEach(category => category.addEventListener('click', function () {
-	document.querySelector('.top-button .active').classList.remove('active');
+	// document.querySelector('.top-button .active').classList.remove('active');
 	category.classList.add('active');
-
+	allCards.forEach(card => {
+		if (category.id === 'all') {
+			card.classList.remove('hide')
+			return;
+		}
+		if (category.id !== card.getAttribute('data-name')) {
+			card.classList.add('hide')
+		} else {
+			card.classList.remove('hide')
+		}
+	})
+}))
+buttons.forEach(category => category.addEventListener('click', function () {
+	category.classList.add('active');
 	allCards.forEach(card => {
 
 		if (category.id === 'all') {
@@ -21,34 +33,7 @@ buttons.forEach(category => category.addEventListener('click', function () {
 
 	})
 }))
-
-
-
-
-buttons.forEach(category => category.addEventListener('click', function () {
-	// document.querySelector('.top-buttonphone .active').classList.remove('active');
-	category.classList.add('active');
-
-	allCards.forEach(card => {
-
-		if (category.id === 'all') {
-			card.classList.remove('hide')
-			return;
-		}
-		if (category.id !== card.getAttribute('data-name')) {
-			card.classList.add('hide')
-		} else {
-			card.classList.remove('hide')
-		}
-
-	})
-}))
-
-
-
 phonePars.addEventListener('click', () => {
-	// topbtn.style.display = "none !important"
 	topbtn.style.display = "block !important";
 	topbtn.classList.toggle("heid")
-	console.log(topbtn)
 })
